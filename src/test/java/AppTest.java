23f7d2b600316218e03beea9fe3ac5fa5d0d1570
Kopysh <dimakopyshov@gmail.com>
2023-03-20 14:03:23 +0300

    Add Database Handler and test it

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,25 +1,25 @@
+import com.kharizma.tennisscoreboard.DBHandler;
+import com.kharizma.tennisscoreboard.models.Match;
 import com.kharizma.tennisscoreboard.models.Player;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 public class AppTest {
-    EntityManagerFactory emf;
-    EntityManager entityManager;
+    public static EntityManager entityManager;
+    Player s1;
+    Player s2;
 
     @BeforeEach
     public void initializeEMF() {
-        emf = Persistence.createEntityManagerFactory("Player_PU");
-        entityManager = emf.createEntityManager();
+        entityManager = DBHandler.getEntityManager();
     }
 
-    @AfterEach
-    public void closeEMF() {
+    @AfterAll
+    public static void closeEMF() {
         entityManager.close();
     }
 
@@ -28,10 +28,10 @@ public class AppTest {
 
         entityManager.getTransaction().begin();
 
-        Player s1 = new Player();
+        s1 = new Player();
         s1.setName("Dima");
 
-        Player s2 = new Player();
+        s2 = new Player();
         s2.setName("Paulina");
 
         entityManager.persist(s1);
@@ -40,8 +40,31 @@ public class AppTest {
         entityManager.getTransaction().commit();
 
         List<Player> list = entityManager.createQuery("from Player ").getResultList();
-        list.forEach(p -> System.out.println(p.getName()));
+        list.forEach(p -> System.out.println("Name " + p.getName() + " and Id " + p.getId()));
+    }
 
+    @Test
+    public void addSomeMatches() {
+        entityManager.getTransaction().begin();
 
+        s1 = new Player();
+        s1.setName("Dima");
+
+        s2 = new Player();
+        s2.setName("Paulina");
+
+        entityManager.persist(s1);
+        entityManager.persist(s2);
+
+        Match match1 = new Match();
+        match1.setPlayerOne(s1);
+        match1.setPlayerTwo(s2);
+        match1.setWinner(s1);
+        entityManager.persist(match1);
+
+        entityManager.getTransaction().commit();
+
+        List<Match> list = entityManager.createQuery("from Match ").getResultList();
+        list.forEach(m -> System.out.println("Id " + m.getId() + ", " + m.getPlayerOne() + ", " + m.getPlayerTwo() + " Winner " + m.getWinner()));
     }
 }
